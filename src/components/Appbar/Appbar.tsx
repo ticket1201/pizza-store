88f12c7fa07d66a710807eb84f1af31657c0fcc6
Anton Metlitsky <ticket1201@gmail.com>
2022-10-23 21:41:46 +0400

    feat: add totalPrice, logo buttons to appbar with handlers

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
+import LocalPizzaRoundedIcon from '@mui/icons-material/LocalPizzaRounded';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {useAppSelector} from '../../utils/hooks';
+import {useNavigate} from 'react-router-dom';
 
 const Appbar = () => {
-/*
-    const cartItemsCount = useAppSelector()
-*/
+
+    const totalPrice = useAppSelector(state => state.cart.totalPrice)
+    const navigate = useNavigate()
+    const handleCartClick = () => {
+        navigate('/cart')
+    }
+    const handleLogoClick = () => {
+        navigate('/')
+    }
 
     return (
-        <AppBar position={'static'} sx={{alignItems: 'flex-end'}} color={'default'}>
-            <IconButton sx={{width: '40px', position:'relative'}}>
-                <ShoppingCartOutlinedIcon/>
-            </IconButton>
+        <AppBar position={'fixed'}
+                color={'default'}>
+            <div className={'headerInner'}>
+                <IconButton onClick={handleLogoClick}>
+                    <LocalPizzaRoundedIcon fontSize={'large'}/>
+                </IconButton>
+                <IconButton sx={{width: 'fit-content', position: 'relative'}} onClick={handleCartClick}>
+                    <ShoppingCartOutlinedIcon/>
+                    {totalPrice && totalPrice + ' $ '}
+                </IconButton>
+            </div>
         </AppBar>
     );
 };
