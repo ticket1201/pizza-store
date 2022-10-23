95ccfa056c8adeed19709ce3bfe8bf2db8baf9e3
Anton Metlitsky <ticket1201@gmail.com>
2022-10-23 21:37:16 +0400

    feat: add cartReducer, actions to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,12 @@ import {combineReducers, configureStore, ThunkAction, ThunkDispatch} from '@redu
 import thunk from 'redux-thunk';
 import {appActions, appReducer} from './appReducer';
 import {productsReducer} from '../feature/PizzaList/productsReducer';
+import {CartActions, cartReducer} from '../feature/Cart/cartReducer';
 
 const rootReducer = combineReducers({
     app: appReducer,
-    products: productsReducer
+    products: productsReducer,
+    cart: cartReducer
 })
 
 export const store = configureStore({
@@ -13,7 +15,7 @@ export const store = configureStore({
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
 })
 
-export type RootActionsType = appActions
+export type RootActionsType = appActions | CartActions
 
 export type RootStateType = ReturnType<typeof store.getState>
 export type RootDispatchType = ThunkDispatch<RootStateType, unknown, RootActionsType>
