ea41d9f994439e3bbbc84cd2127c046c875f9107
Anton Metlitsky <ticket1201@gmail.com>
2022-10-23 21:34:31 +0400

    feat: impl success component

---
+++ src/components/Success/Success.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Success = () => {
+    return (
+        <div className={'wrapper'}>
+            <h1>You Successfully ordered your pizza. Please, wait for confirmation call.</h1>
+        </div>
+    );
+};
+
+export default Success;
