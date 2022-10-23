4d216e718257f6e1291573fd8f60ca0c1b86f484
Anton Metlitsky <ticket1201@gmail.com>
2022-10-23 21:42:43 +0400

    feat: add hashRouter to application

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
+import {HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <App/>
+        <HashRouter>
+            <App/>
+        </HashRouter>
     </Provider>
 );
 
