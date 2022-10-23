ec4a97f5e591a9251ccdd07f5bd7141f6f4467c3
Anton Metlitsky <ticket1201@gmail.com>
2022-10-23 21:43:04 +0400

    feat: add routes with components to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,21 +4,26 @@ import Appbar from '../components/Appbar/Appbar';
 import ProductsList from '../feature/PizzaList/ProductsList';
 import {useAppSelector} from '../utils/hooks';
 import LoadingScreen from '../components/LoadingScreen/LoadingScreen';
+import {Route, Routes} from 'react-router-dom';
+import Cart from '../feature/Cart/Cart';
+import Success from '../components/Success/Success';
 
 
 function App() {
     const isInitialized = useAppSelector(state => state.app.isInitialized)
 
-    if(!isInitialized){
+    if (!isInitialized) {
         return <LoadingScreen isDarken={false}/>
     }
 
     return (
         <div className="App">
             <Appbar/>
-            <div className={'ProductsWrapper'}>
-                <ProductsList/>
-            </div>
+            <Routes>
+                <Route path={'/'} element={<ProductsList/>}/>
+                <Route path={'/cart'} element={<Cart/>}/>
+                <Route path={'/success'} element={<Success/>}/>
+            </Routes>
         </div>
     );
 }
