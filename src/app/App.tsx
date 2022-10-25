33f0c4f2594028b4772e8f8da26ba69f0ffc801b
Anton Metlitsky <ticket1201@gmail.com>
2022-10-25 22:18:03 +0400

    feat: del uninitialized loading screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,12 @@ import React from 'react';
 import './App.css';
 import Appbar from '../components/Appbar/Appbar';
 import ProductsList from '../feature/PizzaList/ProductsList';
-import {useAppSelector} from '../utils/hooks';
-import LoadingScreen from '../components/LoadingScreen/LoadingScreen';
 import {Route, Routes} from 'react-router-dom';
 import Cart from '../feature/Cart/Cart';
 import Success from '../components/Success/Success';
 
 
 function App() {
-    const isInitialized = useAppSelector(state => state.app.isInitialized)
-
-    if (!isInitialized) {
-        return <LoadingScreen isDarken={false}/>
-    }
-
     return (
         <div className="App">
             <Appbar/>
